2ada98f6ce384c0707ae8ebc8a2d56960beb1d3b
dhruv limbasiya <limbasiyadhruv735@gmail.com>
2026-07-22 10:46:34 +0530

    Time: 2051 ms (5.09%), Space: 26.1 MB (49.36%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,24 +1,42 @@
-class Solution
-{
-    public:
-        vector<vector < string>> groupAnagrams(vector<string> &strs)
-        {
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
 
-            map<string, vector < string>> map;
+        vector<vector<string>> ans;
+        vector<bool> visited(strs.size(), false);
 
-            for (int i = 0; i < strs.size(); i++)
-            {
-                string temp = strs[i];
+        // Precompute frequency of every string
+        vector<vector<int>> freq(strs.size(), vector<int>(26, 0));
 
-                sort(temp.begin(), temp.end());
-                map[temp].push_back(strs[i]);
+        for (int i = 0; i < strs.size(); i++) {
+            for (char c : strs[i]) {
+                freq[i][c - 'a']++;
             }
-            vector<vector < string>> ans;
-
-            for (auto it: map)
-            {
-                ans.push_back(it.second);
-            }
-            return ans;
         }
+        
+        for (int i = 0; i < strs.size(); i++) {
+
+            if (visited[i])
+                continue;
+
+            vector<string> temp;
+            temp.push_back(strs[i]);
+            visited[i] = true;
+
+            for (int j = i + 1; j < strs.size(); j++) {
+
+                if (visited[j])
+                    continue;
+
+                if (freq[i] == freq[j]) {
+                    temp.push_back(strs[j]);
+                    visited[j] = true;
+                }
+            }
+
+            ans.push_back(temp);
+        }
+
+        return ans;
+    }
 };
