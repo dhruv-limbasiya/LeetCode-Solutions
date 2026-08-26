d20ae1e7b56f1a1c07457e543c37b02dca1cc64d
dhruv limbasiya <limbasiyadhruv735@gmail.com>
2026-08-26 23:27:56 +0530

    Time: 0 ms (100%), Space: 19.3 MB (50.17%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        s = sentence.lower()
+
+        for i in "abcdefghijklmnopqrstuvwxyz":
+            if i not in s:
+                return False
+        else:
+            return True        
