e578583002bc46c7b5c930d27df1ce9b28ea7c07
dhruv limbasiya <limbasiyadhruv735@gmail.com>
2026-09-05 01:14:35 +0530

    Time: 619 ms (57.52%), Space: 0B (100%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,11 @@
+select employee_id 
+from employees
+where employee_id not in (select employee_id from salaries)
+
+UNION
+
+select employee_id
+from salaries 
+where employee_id not in (select employee_id from employees)
+
+order by employee_id;
