dd3cada3c86e7e53c26a628bfa5375584d726038
dhruv limbasiya <limbasiyadhruv735@gmail.com>
2026-09-11 19:02:44 +0530

    Time: 272 ms (55.56%), Space: 0B (100%) - LeetHub

---
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -0,0 +1,7 @@
+select a.machine_id, 
+        round(
+            (select avg(a1.timestamp) from activity a1 where activity_type = 'end' and a1.machine_id = a.machine_id) - 
+            (select avg(a1.timestamp) from activity a1 where activity_type = 'start' and a1.machine_id = a.machine_id)
+            ,3)as processing_time
+from activity a
+group by a.machine_id
