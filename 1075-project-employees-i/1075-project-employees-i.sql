c8d4171da4b48b90a5471631884d0f6110bda23b
dhruv limbasiya <limbasiyadhruv735@gmail.com>
2026-09-18 11:48:04 +0530

    Time: 503 ms (91%), Space: 0B (100%) - LeetHub

---
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -0,0 +1,5 @@
+select p.project_id, round(sum(e.experience_years) / count(e.experience_years), 2) as average_years 
+from project p
+left join employee e 
+on p.employee_id  = e.employee_id
+group by p.project_id;
