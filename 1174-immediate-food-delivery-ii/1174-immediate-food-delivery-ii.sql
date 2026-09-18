7f11c11d288f9bdc879f5a1ae0caa54f0d3bee94
dhruv limbasiya <limbasiyadhruv735@gmail.com>
2026-09-18 13:18:54 +0530

    Time: 540 ms (99.88%), Space: 0B (100%) - LeetHub

---
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,8 @@
+select 
+    round(sum(if(order_date = customer_pref_delivery_date, 1, 0)) * 100 / count(distinct customer_id) , 2) 
+    as immediate_percentage 
+from delivery
+where 
+    (customer_id, order_date) IN (select customer_id, min(order_date) as first_order_date
+                                from delivery
+                                group by customer_id);
