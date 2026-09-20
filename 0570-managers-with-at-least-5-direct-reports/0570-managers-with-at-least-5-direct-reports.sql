e40af5726c6fdd525f0115540fba266fafbafda4
dhruv limbasiya <limbasiyadhruv735@gmail.com>
2026-09-20 19:51:26 +0530

    Time: 334 ms (94.89%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,6 +1,6 @@
-select e.name
-from employee e
-inner join employee e2
-on e.id = e2.managerid
-group by e.id
+select e1.name
+from employee e1
+join employee e2
+on e1.id = e2.managerid
+group by e1.id
 having count(e2.managerid) >= 5;
