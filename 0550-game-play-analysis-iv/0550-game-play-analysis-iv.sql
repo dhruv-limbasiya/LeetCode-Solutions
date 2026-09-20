0f9e0d8a21527b92674db6ff4538dc72eb37073b
dhruv limbasiya <limbasiyadhruv735@gmail.com>
2026-09-20 18:57:20 +0530

    Time: 636 ms (49.1%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,5 +1,7 @@
-select round(count(distinct player_id) / (select count(distinct player_id) from activity),2) as fraction 
-from activity 
-where (player_id,date_sub(event_date, interval 1 day)) IN (select player_id, min(event_date)
-                                                            from activity
-                                                            group by player_id)
+select 
+    round(count(player_id) / (select count(distinct player_id) from activity),2) as fraction  
+from activity
+where (player_id, date_sub(event_date, interval 1 DAY)) 
+IN (select player_id, min(event_date)
+    from activity
+    group by player_id);
