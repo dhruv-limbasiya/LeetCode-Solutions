2f3d15eb514638db19ab2de1186fcb1f4489cf12
dhruv limbasiya <limbasiyadhruv735@gmail.com>
2026-09-21 09:51:39 +0530

    Time: 554 ms (85.13%), Space: 0B (100%) - LeetHub

---
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -0,0 +1,7 @@
+select employee_id, department_id
+from employee 
+where employee_id IN (select employee_id
+                      from employee
+                      group by employee_id
+                      having count(*) = 1) OR primary_flag = 'Y'
+order by employee_id;     
