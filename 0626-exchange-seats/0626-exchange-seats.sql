7909926be3b5ad77e85cfee78edafa72e31f82ed
dhruv limbasiya <limbasiyadhruv735@gmail.com>
2026-09-24 22:54:10 +0530

    Time: 364 ms (72.96%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,9 +1,11 @@
 select 
     case 
-        when id %2 = 1 and id = (select max(id) from Seat) then id
-        when id %2 = 1 then id + 1
-        else id - 1
-    end as id,
-    student
-from Seat 
-order by id;    
+        when id = (select max(id) from seat) and mod(id, 2) = 1 then id
+    
+        when mod(id, 2) = 1 then id + 1
+
+    else
+        id - 1
+    end as id, student
+from seat
+order by id;            
