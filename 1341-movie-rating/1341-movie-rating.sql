971498fbb9d9c8c9762ca3e3523636600b643343
dhruv limbasiya <limbasiyadhruv735@gmail.com>
2026-09-25 14:30:20 +0530

    Time: 1271 ms (91.37%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,17 +1,17 @@
-(select u.name as results from users u
-join movierating m 
-on u.user_id = m.user_id 
-group by u.name, u.user_id
-order by count(*) DESC, u.name ASC
+(select name as results
+from users join movierating using(user_id)
+group by user_id
+order by count(rating) DESC, name
 limit 1)
 
 UNION ALL
-(
-select m.title as results
-from movies m
-join movierating mr 
-on m.movie_id = mr.movie_id
-where created_at between '2020-02-01' and '2020-02-29'
-group by m.movie_id, m.title
-order by avg(rating) DESC, m.title ASC
-LIMIT 1);
+
+(select title as results
+from movies join movierating using(movie_id)
+where year(created_at) = "2020" and month(created_at) = "02"
+group by movie_id
+order by avg(rating) DESC, title
+limit 1)
+
+
+-- created_at between "2020-01-31" and "2020-03-01"
